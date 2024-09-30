9c2ff91d92137f1122f6324415b525f69b92ca45
Hoangbonduy <57250363+Hoangbonduy@users.noreply.github.com>
2024-09-30 18:09:00 +0700

    Update utf-8 in setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
         'numpy>=2.1.1',
         'sympy>=1.11.1'
     ],
-    long_description=Path("README.md").read_text(),
+    long_description=Path("README.md").read_text(encoding='utf-8'),  # Đặt mã hóa UTF-8
     long_description_content_type="text/markdown",
 )
