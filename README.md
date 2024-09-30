785748b1c53ff4ad0dc4b0efdee846997eca7a2f
Hoangbonduy <57250363+Hoangbonduy@users.noreply.github.com>
2024-09-30 17:49:25 +0700

    Fix setup.py and Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# HUST_PPS
+# Tên Dự Án
+Code cho môn phương pháp số tại Đại học Bách khoa Hà Nội
+## Cài đặt
+pip install HUST_PPS
--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+# Đọc nội dung từ README.md
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='HUST_PPS',
     version='0.1.0',
     packages=find_packages(),
-    install_requires = [
+    install_requires=[
         'numpy>=2.1.1',
         'sympy>=1.11.1'
     ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # Xác định loại nội dung là Markdown
 )
